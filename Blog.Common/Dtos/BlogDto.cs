e5b90c793a7f2858badb7004e8d449e7184537d9
fuga002 <159753999+fuga002@users.noreply.github.com>
2024-05-17 22:06:01 +0500

    BlogDto was filled

--- Blog.Common/Dtos/BlogDto.cs
+++ Blog.Common/Dtos/BlogDto.cs
@@ -2,4 +2,11 @@
 
 public class BlogDto
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public Guid UserId { get; set; }
+
+    //public List<PostDto>? Posts { get; set; }
 }
