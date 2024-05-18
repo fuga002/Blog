fd89a9080bb9a10d56e84ac6a253075e50bafd70
Saydullayev71 <saydullayev_jahongir@mail.ru>
2024-05-18 18:31:20 +0500

    CreatePostModel

---
+++ Blog.Common/Models/Post/CreatePostModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Blog.Common.Models.Post
+{
+    public class CreatPostModel
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public string RileUrl { get; set; }
+        [Required]
+        public string AuthorFullName { get; set; }
+    }
+}
