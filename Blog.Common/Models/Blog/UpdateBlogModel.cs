1833b822e115abfa3418cbf982c59c7a859e0c77
asadbek20005-rgb <shermatovasadbek777@gmail.com>
2024-05-18 21:01:11 +0500

    UpdateBlogModel is created

---
+++ Blog.Common/Models/Blog/UpdateBlogModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Common.Models.Blog
+{
+    public class UpdateBlogModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
