fa38f8fb67d34a962648fb91cda65e6d29cea20f
HusanjonDeveloper <120047912+HusanjonDeveloper@users.noreply.github.com>
2024-05-18 14:55:15 +0500

    Parse Extension for PostDto

---
+++ Blog.Common/Dtos/PostDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Common.Dtos
+{
+    public class PostDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public string FileUrl { get; set; }
+
+        public string AuthorFullName { get; set; }
+
+        public int BlogDtoId { get; set; }
+        public BlogDto? BlogDto { get; set; }
+    }
+}
