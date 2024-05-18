cd18937332a9023f4d837de9e5e917f926199ade
asadbek20005-rgb <shermatovasadbek777@gmail.com>
2024-05-18 17:30:03 +0500

    BlogService and IBlogrepository are created.

---
+++ Blog.Services/Api/BlogService.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Data.Repositories;
+
+namespace Blog.Services.Api
+{
+    public class BlogService
+    {
+        readonly IBlogRepository _blogRepository;
+        public BlogService(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+    }
+}
