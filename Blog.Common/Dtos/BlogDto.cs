292fa7a9bb21f17db394f687fdbd2dc2b3e1f907
Saydullayev71 <saydullayev_jahongir@mail.ru>
2024-05-18 17:56:56 +0500

    BlogDto,PostDtodagi conflictlar tog'irlandi

--- Blog.Common/Dtos/BlogDto.cs
+++ Blog.Common/Dtos/BlogDto.cs
@@ -5,7 +5,6 @@ public class BlogDto
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public DateTime CreatedDate { get; set;} = DateTime.UtcNow;
-    public UserDto? User { get; set; }
+    public Guid UserId { get; set; }
     public List<PostDto>? Posts { get; set; }
 }
