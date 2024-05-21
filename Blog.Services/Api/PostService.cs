398bf301a6dd760eec7e90505af02121675e5f0c
asadbek20005-rgb <shermatovasadbek777@gmail.com>
2024-05-21 13:51:42 +0500

    Relevant Posts are done

--- Blog.Services/Api/PostService.cs
+++ Blog.Services/Api/PostService.cs
@@ -16,13 +16,28 @@ namespace Blog.Services.Api
         }
 
         // These method is not relevant to user and blog
-        public async Task<List<PostDto>> GetAllPosts(Guid userId, int blogId)
+        public async Task<List<PostDto>> GetAllPosts()
         {
             var allPosts = await _postRepository.GetAll();
             return allPosts.ParseModels();
         }
 
+        // These method is relevant to user and blog
+        public async Task<List<PostDto>> GetAllPosts(Guid userId, int blogId)
+        {
+            var allPosts = await _postRepository.GetAll();
+            var filteredPosts = await CheckPosts(userId, blogId, allPosts);
+            return filteredPosts.ParseModels();
+        }
 
-      
+        private async Task<List<Post>> CheckPosts(Guid userId, int blogId, List<Post>? posts)
+        {
+            if (posts is null || posts.Count == 0) throw new Exception("The Posts are null!");
+            var user = await _userRepository.GetById(userId);
+            var blog = user.Blogs?.FirstOrDefault(blog => blog.Id == blogId);
+            var relevantPosts = posts.Where(post => post.BlogId == blogId).ToList();
+            if (relevantPosts is null) throw new Exception("The posts are not relevant!");
+            return relevantPosts;
+        }
     }
 }
