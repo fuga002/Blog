2da4f882bafacb3a37c7a4bdd6f012136fb2afa6
asadbek20005-rgb <shermatovasadbek777@gmail.com>
2024-05-21 12:56:33 +0500

    2-issue is done. Post not to relevant to user and blog is made

--- Blog.Services/Api/PostService.cs
+++ Blog.Services/Api/PostService.cs
@@ -1,13 +1,34 @@
-﻿using Blog.Data.Repositories;
+﻿using Blog.Common.Dtos;
+using Blog.Data.Entities;
+using Blog.Data.Repositories;
+using Blog.Services.Extensions;
 
 namespace Blog.Services.Api
 {
     public class PostService
     {
         readonly IPostRepository _postRepository;
-        public PostService(IPostRepository postRepository)
+        readonly IUserRepository _userRepository;
+        public PostService(IPostRepository postRepository, IUserRepository userRepository)
         {
             _postRepository = postRepository;
+            _userRepository = userRepository;
+        }
+
+        // These method is not relevant to user and blog
+        public async Task<List<PostDto>> GetAllPosts(Guid userId, int blogId)
+        {
+            var allPosts = await _postRepository.GetAll();
+            return allPosts.ParseModels();
+        }
+
+
+        private async Task CheckPosts(Guid userId, int blogId)
+        {
+            var user = await _userRepository.GetById(userId);
+            var blog = user.Blogs?.FirstOrDefault(blog => blog.Id == blogId);
+            var posts = blog?.Posts?.FirstOrDefault(post => post.Id == blogId);
+            if (posts is null) throw new Exception("The posts are not relevant!");
         }
     }
 }
